212032851a6ae70959f7eac833ceb8e0024a0568
parth-sardhara-79 <parthsardhara026@gmail.com>
2019-06-12 18:19:04 +0530

    Custom Pipes for Math function.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExponentialStrengthPipe } from './Pipe/exponential-strength.pipe';
+import { SquarePipe } from './Pipe/square.pipe';
+import { RoundPipe } from './Pipe/round.pipe';
+import { SqrtPipe } from './Pipe/sqrt.pipe';
+import { CeilPipe } from './Pipe/ceil.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExponentialStrengthPipe,
+    SquarePipe,
+    RoundPipe,
+    SqrtPipe,
+    CeilPipe
   ],
   imports: [
     BrowserModule
